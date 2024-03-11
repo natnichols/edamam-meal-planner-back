1feb76c9ff339e6f897f97ed0635e18f816a252e
NatNichols <natalie.e.nichols@gmail.com>
2024-03-11 10:16:19 -0400

    create shoppingListSchema; add recipes, meals, and shoppingList to Profile model

--- models/profile.js
+++ models/profile.js
@@ -2,9 +2,20 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const shoppingListSchema = new Schema({
+  item: String,
+  recipe: {type: Schema.Types.ObjectId, ref: 'Recipe'},
+  purchased: Boolean
+}, {
+  timestamps: true
+})
+
 const profileSchema = new Schema({
   name: String,
-  photo: String
+  photo: String,
+  recipes: [{type: Schema.Types.ObjectId, ref: 'Recipe'}],
+  recipes: [{type: Schema.Types.ObjectId, ref: 'Meal'}],
+  shoppingList: [shoppingListSchema]
 },{
   timestamps: true,
 })
