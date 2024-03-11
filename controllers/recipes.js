9a58d00f05c374f67b7c4b71d2e1f59094c34caa
NatNichols <natalie.e.nichols@gmail.com>
2024-03-11 11:24:14 -0400

    stub up Recipe routes and ctrls, build search ctrl

---
+++ controllers/recipes.js
@@ -0,0 +1,15 @@
+import { Recipe } from "../models/recipe.js"
+
+const edamamUrl = `https://api.edamam.com/api/recipes/v2`
+
+export async function search(req, res) {
+  try {
+    const apiResponse = await fetch(`${edamamUrl}?type=public&q=${req.body.query}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}`)
+    const apiData = await apiResponse.json()
+    apiData.hits.forEach(recipe => delete recipe['_links'] )
+    res.json(apiData.hits)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
---
+++ routes/recipes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+import * as recipesCtrl from '../controllers/recipes.js'
+
+const router = Router()
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+router.use(decodeUserFromToken)
+router.post('/search', checkAuth, recipesCtrl.search)
+
+
+export { router }
